aacfc372d2712a7ec81de822193c42e1834c32c3
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-02-20 10:03:00 +0700

    belajar operator

---
+++ operator.js
@@ -0,0 +1,62 @@
+// Operator unary
+// let age = 25;
+
+// typeof age;
+
+// Binary Operator
+// 5 + 4;
+// 10 / 2;
+// age = 30;
+
+// Ternary Operator
+// age < 18 ? "You are young!" : "Welcome onboard!";
+
+// Assigment Operator
+// Digunakan untuk menginisiasi nilai
+// const name = "Dicoding";
+// let location = "Bandung";
+
+// // Digunakan untuk mengubah nilai
+// location = "Jakarta";
+
+// // Arithmetic Operator
+// 6 + 5; // mengembalikan 11
+// 7 - 2; // mengembalikan 5
+// 8 * 5; // mengembalikan 40
+// 10 / 3; // mengembalikan 3.33
+// 10 % 2; // mengembalikan 0
+// 2(10 + 2); // mengembalikan 24
+
+// Comparison Operator : Operator perbandingan adalah operator yang digunakan untuk membandingkan dua nilai dan mengembalikan nilai boolean (true atau false) sebagai hasil perbandingan
+const a = 10;
+const b = 12;
+
+console.log(a < b); // output: true
+console.log(a > b); // output: false
+
+// Logical Operator
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+console.log(5 === 5 && 3 < 5); // true
+
+// OR
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(5 === 5 || 3 > 5); // true
+
+// NOT
+console.log(!true); // false
+console.log(!false); // true
+
+// String Operator
+const first = "bekerja";
+const second = "sama";
+const merged = first + second;
+
+console.log(merged); // Output: bekerjasama
+console.log(5 == "5");
+console.log((4 * (3 + 5)) / 2);
