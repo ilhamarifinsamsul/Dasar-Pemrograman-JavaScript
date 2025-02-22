5855832d9e7ac2256e4b704648e2c3b419f1c6bc
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-02-22 09:15:12 +0700

    belajar map

---
+++ map.js
@@ -0,0 +1,30 @@
+// membuat map()
+// const map = new Map()
+
+// const productMap = new Map([
+//   ["shoes", 500],
+//   ["cap", 100],
+//   ["jeans", 20],
+// ]);
+// console.log(productMap);
+
+// Menyimpan Nilai di Map
+// const map = new Map();
+// map.set("name", "aras");
+// console.log(map);
+
+// const map2 = new Map();
+// map2.set(1, "number one");
+// console.log(map2);
+
+// Mengakses Nilai di Map
+// const map = new Map();
+// map.set("name", "Ilham");
+// console.log(map.get("name"));
+
+// Menghapus Nilai di Map
+const map = new Map();
+map.set("name", "Ilham");
+map.set("last name", "Arifin");
+map.delete("name");
+console.log(map);
