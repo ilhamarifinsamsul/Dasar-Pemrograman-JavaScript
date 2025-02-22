5797c6c94e05cb1ecbc4bec3811c57400f885204
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-02-22 09:03:49 +0700

    belajar array

---
+++ array.js
@@ -0,0 +1,72 @@
+// const array = [1, 2];
+// console.log(typeof array);
+
+// Membuar array
+// Menggunakan object constructor
+// const users = new Array();
+// const numbers = new Array(5);
+// console.log(users);
+// console.log(numbers);
+
+// Menggunakan sintaks Array.from
+// const foo = Array.from("foo");
+// console.log(foo);
+
+// const users = new Array("Ilham", "Jane", "Jack", "jill");
+// const customers = Array.from(users);
+// console.log(customers);
+
+// Menggunakan array literal
+// const array = [];
+// const fruits = ["apple", "banana", "cherry"];
+// console.log(fruits);
+
+// Mengakses Element Array
+// const myArray = [32, 22, 44, 55];
+// console.log(myArray[2]);
+
+// Manipulasi Nilai Array
+// Menggunakan indexing
+// const myArray = [32, 22, 44, 55];
+// myArray[0] = 20;
+// console.log(myArray);
+
+// Menggunakan push
+// const myArray = [32, 22, 44, 55];
+// myArray.push(5);
+// console.log(myArray);
+
+// Menghapus Element dan Data Array
+// const myArray = ["Ilham", "Anisa", "Epul"];
+// delete myArray[2];
+// console.log(myArray);
+
+// Menggunakan method spilice()
+// const myArray = ["Ilham", "Anisa", "Epul", "Ica", "Ramon"];
+// myArray.splice(2, 3);
+// console.log(myArray);
+
+// Menggunakan method shift, pop
+// const myArray = ["Ilham", "Anisa", "Epul", "Ica", "Ramon"];
+// myArray.shift();
+// console.log(myArray);
+
+// const myArray = ["Ilham", "Anisa", "Epul", "Ica", "Ramon"];
+// myArray.pop();
+// console.log(myArray);
+
+// Array Destructuring:  untuk melihat isi dari array
+// const introduction = ["Hello", "Ilham"];
+// const [greeting, name] = introduction;
+// console.log(greeting, name);
+
+// Array Method
+// Mthod reverse -> mengembalikain nilai array yang dibalik
+// const myArray = ["Android", "Data Science", "Web"];
+// myArray.reverse();
+// console.log(myArray);
+
+// method sort -> mengurutkan nilai array
+const myArray = ["Web", "Android", "Data Science"];
+myArray.sort();
+console.log(myArray);
