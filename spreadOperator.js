2e210ec05ad73fd3deb521b5880aa6169fe4f52a
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-02-22 09:30:07 +0700

    belajar spread operator

---
+++ spreadOperator.js
@@ -0,0 +1,22 @@
+// Object
+// const obj1 = { name: "Dicoding" };
+// const obj2 = { lastName: "Indonesia", address: "Jl. Batik Kumeli No 50" };
+// const newObj = { ...obj1, ...obj2 };
+
+// console.log(newObj);
+
+// const originObj = { name: "Dicoding", age: 9 };
+// const copiedObj = { ...originObj };
+// console.log(copiedObj);
+
+// Array
+const array1 = ["Dicoding"];
+const array2 = ["Indonesia", "Jl. Batik Kumeli No 50"];
+const newArray = [...array1, ...array2];
+
+console.log(newArray);
+
+const original = ["apple", "banana", "cherry"];
+const copy = [...original];
+
+console.log(copy);
