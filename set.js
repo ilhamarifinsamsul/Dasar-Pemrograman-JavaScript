c8bb2f405ed6c3ffb87ca5db1c187ea32a500b25
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-02-22 09:23:46 +0700

    belajar set

---
+++ set.js
@@ -0,0 +1,40 @@
+// membuat set
+// const set = new Set()
+
+// const mySet = new Set([1, 2, 3]);
+// console.log(mySet);
+
+// Menyimpan Nilai di Set
+// const set = new Set();
+// set.add(1);
+// set.add("Apple");
+// set.add(2);
+// set.add("Orange");
+// console.log(set);
+
+// Mengakses Nilai di Set
+// const set = new Set();
+// set.add(1);
+// set.add(2);
+// set.add(3);
+
+// for (const number of set) {
+//   console.log(number);
+// }
+
+// const set = new Set();
+// set.add(1);
+// set.add(2);
+// set.add(3);
+
+// set.forEach((element) => {
+//   console.log(element);
+// });
+
+// Menghapus Nilai di Set
+const set = new Set();
+set.add(1);
+set.add(2);
+set.delete(1);
+
+console.log(set);
